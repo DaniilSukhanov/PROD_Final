d6148f13daf1719d8cf96c68add25ac11005e168
DaniilSukhanov <82699248+DaniilSukhanov@users.noreply.github.com>
2024-03-30 22:01:51 +0300

    Initial Commit

---
+++ TabBed/TabBedApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabBedApp.swift
+//  TabBed
+//
+//  Created by Даниил Суханов on 30.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct TabBedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
