a30068afa7713947b88b509ea3d3d1a25de0a08a
DaniilSukhanov <82699248+DaniilSukhanov@users.noreply.github.com>
2024-04-03 01:27:58 +0300

    finish

---
+++ TabBed/Component/Agent/DetailAgentView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailAgentView.swift
+//  TabBed
+//
+//  Created by Даниил Суханов on 02.04.2024.
+//
+
+import SwiftUI
+
+struct DetailAgentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailAgentView()
+}
